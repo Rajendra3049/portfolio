94b57dcea8a31b52302d27fafa6d9217e60f9eae
Rajendra Patel <rajendrapatel3049@gmail.com>
2026-06-28 23:14:14 +0530

    Refactor resume redirect to use dynamic file ID for improved link management. Update the redirect URL to facilitate direct download of the resume file.

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function ResumePage() {
-  redirect(
-    "https://drive.google.com/file/d/1YVn8XqmTOMYc3KYaUEXthaNggsson0LP/view?usp=drive_link",
-  );
+  const resumeFileId = "1YVn8XqmTOMYc3KYaUEXthaNggsson0LP";
+  redirect(`https://drive.google.com/uc?export=download&id=${resumeFileId}`);
 }
